aa3c0c01427b179dae670d321e94ccba9a1097eb
yakar <yakar@live.com>
2017-08-02 18:42:52 +0300

    ayarlar.conf: masaüstü açıklaması düzenlendi.

---
+++ scripts/de-gnome.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+mesaj bilgi "$MASAUSTU kurulum ve ayarlari yapiliyor.."
+chroot $LFS /bin/bash -c "mps kur $MASAUSTU"
+
+mesaj bilgi "$MASAUSTU gerekli uygulamalar kuruluyor.."
+cp -r $BUILDER_ROOT/$OZELLESTIRME/$MASAUSTU/apps $LFS/tmp
+chroot $LFS /bin/bash -c "mps -kurul /tmp/apps"
+
+# default config
+#cp -r $BUILDER_ROOT/$OZELLESTIRME/$MASAUSTU/.config $LFS/root/
+
+# varsayilan arkaplan
+#cp -r $BUILDER_ROOT/$OZELLESTIRME/$MASAUSTU/backgrounds/milis-linux-arkaplan.png $LFS/sources/milis.git/ayarlar/
+
+# varsayılan milis logo
+#cp -r $BUILDER_ROOT/$OZELLESTIRME/$MASAUSTU/milislogo.png $LFS/sources/milis.git/ayarlar/
+
+# cesitli arkaplanlar
+#cp -r $BUILDER_ROOT/$OZELLESTIRME/$MASAUSTU/backgrounds/* $LFS/usr/share/backgrounds/xfce/
