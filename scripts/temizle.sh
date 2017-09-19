1891bf8b75edc4c911df0e1b3c35c750537b05fe
igor0pav <igor0pavlov@gmail.com>
2017-09-19 11:12:54 +0300

    kapatilan fonk. acildi

--- scripts/temizle.sh
+++ scripts/temizle.sh
@@ -41,7 +41,7 @@ while true; do
 				rm -f iso_icerik/boot/kernel
 
                 if [ -f iso_icerik/efiboot.img ]; then
-				    rm -rf iso_icerik/efiboot.img
+				        rm -rf iso_icerik/efiboot.img
                 fi
 				mesaj bilgi "iso_icerik/LiveOS klasörü silindi."
 			fi
