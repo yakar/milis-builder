7ecb97c2b5cbd802cd76d346709463fa3d81c4a2
milisbir <milisarge@gmail.com>
2018-06-04 11:58:42 +0200

    lightdm

--- de/xfce4/updates/root/bin/canli_kullanici.sh
+++ de/xfce4/updates/root/bin/canli_kullanici.sh
@@ -10,4 +10,3 @@ git config --global http.sslCAinfo /etc/ssl/ca-bundle.crt
 mv /.gitconfig /home/canlikullanici/
 rm -rf /etc/rc.d/init.d/lightdm
 mv /etc/rc.d/init.d/lightdm.orj /etc/rc.d/init.d/lightdm
-
